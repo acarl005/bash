5f62ee701e6a5a1674c71fbc2eeb8c0e9bf4fd35
Andrew Carlson <acarl005@g.ucla.edu>
2015-12-03 10:58:14 -0800

    add apt packages

---
+++ packages.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install git unity-tweak-tool ubuntu-restricted-extras google-chrome-stable conky-all gimp tilda redshift default-jre xclip espeak simplescreenrecorder exfat-fuse exfat-utils traceroute
+
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-add-repository -y ppa:rael-gc/scudcloud
+sudo apt-get update
+sudo apt-get install sublime-text dorian-theme-gtk3 scudcloud
