d307e8e9d10fdd0dc6ccbc4ebd6a3b26a1be76b2
Andy <acarl005@g.ucla.edu>
2016-01-26 09:58:38 -0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,5 @@
 sudo apt-get update &&
-sudo apt-get install git unity-tweak-tool ubuntu-restricted-extras google-chrome-stable conky-all gimp tilda redshift default-jre xclip espeak simplescreenrecorder exfat-fuse exfat-utils traceroute ttf-ancient-fonts &&
-
+sudo apt-get install git unity-tweak-tool ubuntu-restricted-extras google-chrome-stable conky-all gimp tilda redshift redshift-gtk default-jre xclip espeak simplescreenrecorder exfat-fuse exfat-utils traceroute ttf-ancient-fonts &&
 sudo add-apt-repository ppa:webupd8team/sublime-text-2 &&
 sudo add-apt-repository ppa:noobslab/themes &&
 sudo apt-add-repository -y ppa:rael-gc/scudcloud &&
