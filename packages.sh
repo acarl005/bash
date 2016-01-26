559880004f96c45941958f7cea7f64cfb83d4618
Andrew Carlson <acarl005@g.ucla.edu>
2016-01-26 11:04:25 -0800

    add guake

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 sudo apt-get update &&
-sudo apt-get install git unity-tweak-tool ubuntu-restricted-extras conky-all gimp tilda redshift redshift-gtk default-jre xclip espeak exfat-fuse exfat-utils traceroute ttf-ancient-fonts &&
+sudo apt-get install git unity-tweak-tool ubuntu-restricted-extras conky-all gimp tilda redshift redshift-gtk default-jre xclip guake espeak exfat-fuse exfat-utils traceroute ttf-ancient-fonts &&
 sudo add-apt-repository ppa:webupd8team/sublime-text-2 &&
 sudo add-apt-repository ppa:noobslab/themes &&
 sudo apt-add-repository -y ppa:rael-gc/scudcloud &&
