098ee6185aca19ce485fef8cfdcfcc47722557ad
Andy <acarl005@g.ucla.edu>
2016-01-26 10:01:35 -0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,7 +1,8 @@
 sudo apt-get update &&
-sudo apt-get install git unity-tweak-tool ubuntu-restricted-extras google-chrome-stable conky-all gimp tilda redshift redshift-gtk default-jre xclip espeak simplescreenrecorder exfat-fuse exfat-utils traceroute ttf-ancient-fonts &&
+sudo apt-get install git unity-tweak-tool ubuntu-restricted-extras conky-all gimp tilda redshift redshift-gtk default-jre xclip espeak exfat-fuse exfat-utils traceroute ttf-ancient-fonts &&
 sudo add-apt-repository ppa:webupd8team/sublime-text-2 &&
 sudo add-apt-repository ppa:noobslab/themes &&
 sudo apt-add-repository -y ppa:rael-gc/scudcloud &&
+sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder &&
 sudo apt-get update &&
-sudo apt-get install sublime-text dorian-theme-gtk3 scudcloud
+sudo apt-get install sublime-text dorian-theme-gtk3 scudcloud simplescreenrecorder
