4bb48dda916e0308b107d2cbaf4fa8195463334c
Andrew Carlson <acarl005@g.ucla.edu>
2016-02-23 15:08:12 -0800

    remove .atom dir

--- .atom/compile-cache/coffee/da39a3ee5e6b4b0d3255bfef95601890afd80709.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL2hvbWUvYW5keS8uYXRvbS9wYWNrYWdlcy9maWxlLWljb25zL2V4YW1wbGVzL2NvZmZlZXNjcmlwdC5jb2ZmZWUiCiAgXSwKICAibmFtZXMiOiBbXSwKICAibWFwcGluZ3MiOiAiQUFBQTtBQUFBO0FBQUEiCn0=
-
-//# sourceURL=/home/andy/.atom/packages/file-icons/examples/coffeescript.coffee
